eb447ea545802444628d79e8cfb47cb36dc21a45
ghoshtanmay87 <ghoshtanmay87@gmail.com>
2017-02-22 16:07:34 +0530

    Seat related files

--- src/main/java/org/samvitticapital/model/Request.java
+++ src/main/java/org/samvitticapital/model/Request.java
@@ -1,51 +1,44 @@
 package org.samvitticapital.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.springframework.stereotype.Component;
-
-@Component
 @Entity
-@Table(name = "tbl_request")
-public class Request {
-
+@Table(name="tbl_request")
+public class Request 
+{
 	@Id
 	@GeneratedValue
-	@Column(name = "request_id")
-	int requestId;
-	@Column(name = "project_id")
-	int projectId;
-	@Column(name = "type_of_request")
-	String typeOfRequest;
-	@Column(name = "request_status")
-	String requestStatus;
-	@Column(name = "no_of_seats")
-	int noOfSeats;
-	@Column(name = "preferred_bay")
-	String preferredBay;
-	@Column(name = "apprvl_leadership")
-	String leadershipApprvl;
-	@Column(name = "request_comment")
-	String requestComment;
-
+	@Column(name="request_id")
+	private int requestId;
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="proj_id", referencedColumnName="project_id")
+	private Project project;
+	@Column(name="type_of_request")
+	private String typeOfRequest;;
+	@Column(name="no_of_seats")
+	private int noOfSeats;
+	@Column(name="preferred_bay")
+	private String preferredBay;
+	@Column(name="apprvl_leadership")
+	private String leadershipApprvl;
+	@Column(name="request_comment")
+	private String requestComment;
+	@Column(name="request_status")
+	private String requestStatus;
+	
 	public int getRequestId() {
 		return requestId;
 	}
 
 	public void setRequestId(int requestId) {
 		this.requestId = requestId;
-	}
-
-	public int getProjectId() {
-		return projectId;
-	}
-
-	public void setProjectId(int projectId) {
-		this.projectId = projectId;
 	}
 
 	public String getTypeOfRequest() {
@@ -94,5 +87,13 @@ public class Request {
 
 	public void setRequestComment(String requestComment) {
 		this.requestComment = requestComment;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
 	}
 }
