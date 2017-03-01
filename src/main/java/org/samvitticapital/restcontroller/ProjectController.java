4dfd9616f8704f6f6400c645fdef0515eb623e79
nkapurgit <nkapur@deloitte.com>
2017-03-01 20:39:26 +0530

    project controller change

--- src/main/java/org/samvitticapital/restcontroller/ProjectController.java
+++ src/main/java/org/samvitticapital/restcontroller/ProjectController.java
@@ -6,11 +6,16 @@ import org.samvitticapital.Dao.ProjectDao;
 import org.samvitticapital.model.Employee;
 import org.samvitticapital.model.Project;
 import org.samvitticapital.vo.GenericResponse;
+import org.samvitticapital.vo.ProjectReq;
 import org.samvitticapital.vo.Projects;
+import org.samvitticapital.vo.RequestParamBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,24 +27,21 @@ public class ProjectController {
 	@Autowired
 	private Project project;
 
-	@RequestMapping("/newProject")
-	public ResponseEntity<GenericResponse> saveNewProject(
-			@RequestParam(value = "projectName", required = true) String projectName,
-			@RequestParam(value = "projectManagerEmailId", required = true) String projectManagerEmailId,
-			@RequestParam(value = "projectDirectorEmailId", required = true) String projectDirectorEmailId,
-			@RequestParam(value = "projectStartDate", required = true) String projectStartDate,
-			@RequestParam(value = "projectEndDate", required = false) String projectEndDate) {
+	@RequestMapping(value = "/newProject", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<GenericResponse> saveNewProject(@RequestBody ProjectReq req) {
 		try {
+			System.out.println("manager email id - "+req.getProjectManagerEmailId());
+			String projectManagerEmailId = req.getProjectManagerEmailId();
 			Employee e = projectDao.getEmployeeDetails(projectManagerEmailId);
 			System.out.println("got emp id - " + e.getEmployeeId());
-			project.setProjectName(projectName);
+			project.setProjectName(req.getProjectName());
 			project.setManagerId(e.getEmployeeId());
-			e = projectDao.getEmployeeDetails(projectDirectorEmailId);
+			e = projectDao.getEmployeeDetails(req.getProjectDirectorEmailId());
 			System.out.println("got emp id 2nd- " + e.getEmployeeId());
 			project.setDirectorId(e.getEmployeeId());
-			project.setStartDate(new Date(projectStartDate));
-			if (null != projectEndDate)
-				project.setEndDate(new Date(projectEndDate));
+			project.setStartDate(new Date(req.getProjectStartDate()));
+			if (null != req.getProjectEndDate())
+				project.setEndDate(new Date(req.getProjectEndDate()));
 			projectDao.save(project);
 			int newProjectId = project.getProjectId();
 			System.out.println("newProjectId - "+newProjectId);
@@ -53,11 +55,11 @@ public class ProjectController {
 		}
 	}
 
-	@RequestMapping(value = "/getProject")
-	public ResponseEntity fetchProject(
-			@RequestParam(value = "projectId", required = true) int projectId) {
+	@RequestMapping(value = "/getProject", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity fetchProject(@RequestBody ProjectReq req) {
 		String empName = "";
 		try {
+			int projectId = req.getProjectId();
 			project = projectDao.get(projectId);
 			int noOfSeats = projectDao.getSeatCount(projectId);
 			project.setNoOfSeats(noOfSeats);
